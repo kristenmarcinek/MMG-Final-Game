123758b5855c706928ef20f20ea80879693a21c5
lemosadam <117216643+lemosadam@users.noreply.github.com>
2023-11-20 13:48:03 -0500

    Implemented Step by Step Activation

--- CookingSim/Assets/Scripts/RecipeManager.cs
+++ CookingSim/Assets/Scripts/RecipeManager.cs
@@ -7,6 +7,9 @@ public class RecipeManager : MonoBehaviour
 {
     public List<float> scoresList;
     public static RecipeManager sharedInstance;
+    public GameObject targetObject;
+    public GameObject[] recipeSteps;
+    public int stepTracker = 0;
 
 
     void Awake()
@@ -20,17 +23,34 @@ public class RecipeManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        recipeSteps = GameObject.FindGameObjectsWithTag("step");
+        
+
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
+       foreach (GameObject step in recipeSteps)
+        {
+            targetObject = step;
+            DeactivateAllChildren();
 
+          
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            NextStep();
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             foreach (float score in scoresList)
@@ -49,6 +69,20 @@ public class RecipeManager : MonoBehaviour
 
             }
 
+        }
+
+        // Check for user input or any other condition to activate the objects
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Activate the objects
+            ActivateAllChildren();
+        }
+
+        // Check for user input or any other condition to deactivate the objects
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Deactivate the objects
+            DeactivateAllChildren();
         }
 
 
@@ -70,5 +104,71 @@ public class RecipeManager : MonoBehaviour
         float average = total / scoresList.Count;
         Debug.Log("the average is:" + average.ToString());
 
+    }
+
+    void ActivateAllChildren()
+    {
+        // Check if the targetObject is assigned
+        if (targetObject != null)
+        {
+            // Activate all children of the targetObject
+            foreach (Transform child in targetObject.transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            Debug.LogError("Target object not assigned");
+        }
+    }
+
+    void DeactivateAllChildren()
+    {
+        // Check if the targetObject is assigned
+        if (targetObject != null)
+        {
+            // Deactivate all children of the targetObject
+            foreach (Transform child in targetObject.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.LogError("Target object not assigned");
+        }
+    }
+
+    void ActivateNextChild()
+    {
+        // Check if the targetObject is assigned
+        if (targetObject != null)
+        {
+            // Check if the stepTracker is within the range of the children count
+            if (stepTracker < targetObject.transform.childCount)
+            {
+                // Activate the next child
+                targetObject.transform.GetChild(stepTracker).gameObject.SetActive(true);
+                // Increment the stepTracker
+                stepTracker++;
+            }
+            else
+            {
+                Debug.Log("No more children to activate");
+            }
+        }
+        else
+        {
+            Debug.LogError("Target object not assigned");
+        }
+    }   
+
+    void NextStep()
+    {
+        DeactivateAllChildren();
+        stepTracker++;
+        targetObject = GameObject.Find("Step " + stepTracker.ToString());
+        ActivateAllChildren();
     }
 }
